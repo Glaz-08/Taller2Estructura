89da7571d5bcf59d014b904279225f35ad6c2c53
Glaz-08 <130676103+Glaz-08@users.noreply.github.com>
2024-05-18 23:34:41 +0000

    hay errores de sintaxis, arreglar

--- Main.cpp
+++ Main.cpp
@@ -3,9 +3,11 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <queue>
+
 using namespace std;
 // Función para leer datos desde un archivo de texto y cargarlos en los vectores de clientes
-void leerDatosClientesTxt(vector<Cliente*>& clientes;){
+void leerDatosClientesTxt(queue<Cliente*>& clientes,queue<Cliente*>& auxiliar){
     string linea;
     ifstream archivo("Clientes.txt");  
     char delimitador = ',';
@@ -18,12 +20,14 @@ void leerDatosClientesTxt(vector<Cliente*>& clientes;){
             string tipoPrioridad=partes[3];
             int numero = stoi(partes[4]);
             Cliente* clientePref = new ClientePreferencial(nombre,edad,tipo,tipoPrioridad,numero);
-            clientes.push_back(clientePref);
+            clientes.push(clientePref);
+            auxiliar.push(clientePref);
         }
         else{
             int numeroNorm = stoi(partes[4]);
             Cliente* clienteNorm = new ClienteNormal(nombre,edad,tipo,numeroNorm);
-            clientes.push_back(clienteNorm);
+            clientes.push(clienteNorm);
+            auxiliar.push(clienteNorm);
         }
 
     }archivo.close();
@@ -31,7 +35,7 @@ void leerDatosClientesTxt(vector<Cliente*>& clientes;){
 
 }
 // Función para leer datos desde un archivo de texto y cargarlos en los vectores de categorias
-void leerDatosClientesTxt(vector<Categoria*>& categorias;){
+void leerDatosClientesTxt(queue<Categoria*>& categorias){
     string linea;
     ifstream archivo("Productos.txt");  
     char delimitador = ',';
@@ -43,20 +47,66 @@ void leerDatosClientesTxt(vector<Categoria*>& categorias;){
         int cantidad=stoi(partes[3]);
         int id=stoi(partes[4]);
         categoria* categoria= new SubCategoria(nombreCategoria,nombreSubCategoria,precio,cantidad,id);
-        categorias.push_back(categoria);
+        categorias.push(categoria);
 
     }archivo.close();
+
+}
+void ordenamientoPorPrioridad(queue<Cliente*>& clientes,queue<Cliente*>& prioridades,queue<Cliente*>& auxiliar){
+    int numeroMenor=0;
+    queue<Cliente*>& auxiliar2;
+    while (!clientes.empty()) {
+        while (!auxiliar.empty()){
+
+            if(clientes.top()->getTipo()=="Preferencial"){
+
+                if(auxiliar.top()->getTipo()=="Preferencial"){
+                    if(clientes.top().getNumero()>=auxiliar.top()){
+                        numeroMenor=auxiliar.top()->getNumero();
+                        auxiliar2.push(auxiliar.top());
+                        auxiliar.pop();
+
+                    }
+                    else{
+                        auxiliar2.push(auxiliar.top());
+                        auxiliar.pop();
+                    }
+
+                }
+            }
+
+        }
+        while (!clientes.empty()){
+            if(clientes.top()->getNumero()==numeroMenor){
+                 prioridades.push(clientes.top());
+            }
+        }
+        while (!auxiliar2.empty()){
+            auxiliar.push(auxiliar2.top());
+            auxiliar2.pop();
+        }
+    }
+}
+void mostrar(queue<Cliente*>& prioridades;){
+    while (!prioridades.empty()){
+        cout << "Atendiendo a: " << cliente->getNumero() << endl;
+
+    }
 
 }
 
 
 
-
 int main(){
-    vector<Cliente*> clientes;
-    vector<Categoria*> categorias;
-    leerDatosClientesTxt(clientes);
-    leerDatosClientesTxt(categorias)
+    queue<Cliente*> clientes;
+    queue<Categoria*> categorias;
+    queue<Cliente*> prioridades;
+    queue<Cliente*> auxiliar;
+    leerDatosClientesTxt(clientes,auxiliar);
+    leerDatosClientesTxt(categorias);
+    ordenamientoPorPrioridad(clientes,prioridades,auxiliar);
+    mostrar(prioridades);
+
 
     
 
